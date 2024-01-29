c3e417deb704ef35545cdbd8c486af0edc6bf654
Nuno Góis <github@nunogois.com>
2024-01-29 12:09:45 +0000

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { FlagProvider, type IConfig } from '@unleash/proxy-client-svelte';
+
+	const config: IConfig = {
+		url: 'https://app.unleash-hosted.com/demo/api/frontend',
+		clientKey:
+			'custom-context-test:default.dfb62cbcda870f2c6516e19dcf40ededd70799d7c9fe50c64e894a26',
+		refreshInterval: 2,
+		appName: 'svelte-sdk-demo',
+		context: {
+			properties: {
+				customContext: 'yes'
+			}
+		}
+	};
+</script>
+
+<FlagProvider {config}>
+	<slot />
+</FlagProvider>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { useFlag, useVariant } from '@unleash/proxy-client-svelte';
+
+	const enabled = useFlag('svelte-test-feature');
+	const variant = useVariant('svelte-test-feature');
+
+	const customContextFlag = useFlag('custom-context-flag');
+
+	$: console.table({ $enabled, $variant });
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+{#if $enabled}
+	<p>New toggle is enabled</p>
+	<p>Variant is {JSON.stringify($variant)}</p>
+{:else}
+	<p>New toggle is disabled</p>
+{/if}
+
+{#if $customContextFlag}
+	<p>Custom context flag is enabled</p>
+{:else}
+	<p>Custom context flag is disabled</p>
+{/if}
